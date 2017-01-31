3bc17e9a858487ba2c5c805c6802d6176b899ef0
Estevao Soares dos Santos <estevao.santos@gmail.com>
2017-01-31 02:16:37 +0000

    add sourcemaps

---
+++ README.md
@@ -0,0 +1,22 @@
+# Shodown Ghost Highlight
+
+Add highglight syntax support to [Showdown][] and [Ghost][]
+
+Can be used with only showdown (without Ghost)
+
+## syntax
+
+To highlight a word or sentence, enclose it with double equal signs `==`
+
+```md
+this is ==an highlighted sentence==.
+```
+
+```html
+<p>this is <mark>an highlighted sentence</mark>.</p>
+```
+
+<p>this is <mark>an highlighted sentence</mark>.</p>
+
+[Showdown]: http://showdownjs.github.io/showdown/
+[Ghost]: https://ghost.org/
---
+++ dist/showdown-ghost-highlight.js
@@ -0,0 +1,58 @@
+;/*! showdown-ghost-highlight 31-01-2017/* jshint node:true, browser:true */
+
+// Ghost Image Preview
+//
+// Manages the conversion of image markdown `![]()` from markdown into the HTML image preview
+// This provides a dropzone and other interface elements for adding images
+// Is only used in the admin client.
+(function (extension) {
+  'use strict';
+
+  if (typeof showdown === 'object') {
+    // global (browser or nodejs global)
+    showdown.extension('highlight', extension());
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD
+    define('highlight', extension());
+  } else if (typeof exports === 'object') {
+    // Node, CommonJS-like
+    module.exports = extension();
+  } else {
+    // showdown was not found so we throw
+    throw Error('Could not find showdown library');
+  }
+
+}(function() {
+  return [
+    // Handle escaped =
+    {
+      type: 'lang',
+      regex: /\\=/g,
+      replace: function (match) {
+        return '¨E' + match.charCodeAt(1) + 'E';
+      }
+    },
+    // Adds highlight syntax as per RedCarpet:
+    //
+    // https://github.com/vmg/redcarpet
+    //
+    // This is ==highlighted==. It looks like this: <mark>highlighted</mark>
+    //
+    // Is only called on span gamut, after italics and bold are parsed,
+    // so words can be highlighted and bolded
+    {
+      type: 'listener',
+      listeners: {
+        'italicsAndBold.after': function (evtName, text) {
+          text = text.replace(/\B==([^=\s][\s\S]*?)==\B/gm, function (match, content) {
+            return '<mark>' + content + '</mark>';
+          });
+          return text;
+        }
+      }
+    }
+  ];
+
+}));
+
+//# sourceMappingURL=showdown-ghost-highlight.js.map
